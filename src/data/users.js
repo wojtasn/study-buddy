540a5c3d896be1c68bdf4944f52d971e28943eb3
wojtasn <wojciech.nachman@gmail.com>
2021-10-22 22:52:21 +0200

    users list final

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    name: 'Wojciech Nachman',
+    attendance: '85%',
+    average: '4.6',
+  },
+  {
+    name: 'Bartłomiej Drozd',
+    attendance: '59%',
+    average: '3.4',
+  },
+  {
+    name: 'Anna Nachman-Osowska',
+    attendance: '75%',
+    average: '4.0',
+  },
+  {
+    name: 'Mateusz Siebyła',
+    attendance: '42%',
+    average: '2.4',
+  },
+  {
+    name: 'Dominik Siekacz',
+    attendance: '23%',
+    average: '1.9',
+  },
+  {
+    average: '1.9',
+  },
+];
